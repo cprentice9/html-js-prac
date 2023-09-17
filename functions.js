54f1e0034320d10fd6fa03d8b6a7feb58beaa784
Connor Prentice <connorprentice@proton.me>
2023-09-16 22:07:28 -0500

    added some fun :)

--- functions.js
+++ functions.js
@@ -1,27 +1,27 @@
 function add() {
-    var numOne = document.getElementById('num1').value;
-    var numTwo = document.getElementById('num2').value;
-    var theAnswer = parseInt(numOne) + parseInt(numTwo);
+    const numOne = document.getElementById('num1').value;
+    const numTwo = document.getElementById('num2').value;
+    const theAnswer = parseInt(numOne) + parseInt(numTwo);
     alert(theAnswer);
-    };
+    }
 
 function subtract() {
-    var numOne = document.getElementById('num1').value;
-    var numTwo = document.getElementById('num2').value;
-    var theAnswer = parseInt(numOne) - parseInt(numTwo);
+    const numOne = document.getElementById('num1').value;
+    const numTwo = document.getElementById('num2').value;
+    const theAnswer = parseInt(numOne) - parseInt(numTwo);
     alert(theAnswer);
-    };
+    }
 
 function multiply() {
-    var numOne = document.getElementById('num1').value;
-    var numTwo = document.getElementById('num2').value;
-    var theAnswer = parseInt(numOne) * parseInt(numTwo);
+    const numOne = document.getElementById('num1').value;
+    const numTwo = document.getElementById('num2').value;
+    const theAnswer = parseInt(numOne) * parseInt(numTwo);
     alert(theAnswer);
-    };
+    }
 
 function divide() {
-    var numOne = document.getElementById('num1').value;
-    var numTwo = document.getElementById('num2').value;
-    var theAnswer = parseInt(numOne) / parseInt(numTwo);
+    const numOne = document.getElementById('num1').value;
+    const numTwo = document.getElementById('num2').value;
+    const theAnswer = parseInt(numOne) / parseInt(numTwo);
     alert(theAnswer);
-    };
+    }
