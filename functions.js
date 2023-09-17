ce1f5cbef477cdf595bc79929392ed0dd61d97c5
Connor Prentice <connorprentice@proton.me>
2023-09-16 20:40:33 -0500

    added functions for math

---
+++ functions.js
@@ -0,0 +1,27 @@
+function add() {
+    var numOne = document.getElementById('num1').value;
+    var numTwo = document.getElementById('num2').value;
+    var theAnswer = parseInt(numOne) + parseInt(numTwo);
+    alert(theAnswer);
+    };
+
+function subtract() {
+    var numOne = document.getElementById('num1').value;
+    var numTwo = document.getElementById('num2').value;
+    var theAnswer = parseInt(numOne) - parseInt(numTwo);
+    alert(theAnswer);
+    };
+
+function multiply() {
+    var numOne = document.getElementById('num1').value;
+    var numTwo = document.getElementById('num2').value;
+    var theAnswer = parseInt(numOne) * parseInt(numTwo);
+    alert(theAnswer);
+    };
+
+function divide() {
+    var numOne = document.getElementById('num1').value;
+    var numTwo = document.getElementById('num2').value;
+    var theAnswer = parseInt(numOne) / parseInt(numTwo);
+    alert(theAnswer);
+    };
